e23cd67127d6a5729720a21b285dc287916df3ff
Will Patterson <pattwm16@gmail.com>
2019-11-23 12:00:00 -0500

    stop back prop through vgg19

--- visual_feature_extractor.py
+++ visual_feature_extractor.py
@@ -1,7 +1,8 @@
 # visual feature extractor
 # Will Patterson
 import keras
-from keras.layers import Dense, Dropout, Flatten, LeakyReLU
+import tensorflow as tf
+from keras.layers import Dense, Dropout, Flatten, LeakyReLU, Lambda
 from keras.models import Model
 
 # this model should take images as input
@@ -11,6 +12,8 @@ from keras.models import Model
 # p is the dimension of the final visual feature representation
 # TODO: Tune this
 p = 32
+# hidden size, defined in paper
+hidden_size = 32
 # number of classes in vgg19
 num_classes = 1000
 # input shape, must be tuple with 3 channels
@@ -24,19 +27,21 @@ vgg19_model = keras.applications.vgg19.VGG19(include_top=False,
                                                 input_shape=inp_shape,
                                                 pooling=None)
 print("VGG19 model instantiated")
-
 x = Flatten()(vgg19_model.output)
-x = Dense(p)(x)
-x = LeakyReLU(alpha=0.3)(x)
-#x = Dropout(0.5)(x)
-#x = BatchNormalization()(x)
-predictions = Dense(num_classes, activation = 'softmax')(x)
+
+# paper prevented back prop through vgg19 model
+x_1_stop_grad = Lambda(lambda x: tf.stop_gradient(x))(x)
+x_1 = Dense(hidden_size)(x_1_stop_grad)
+x_1 = LeakyReLU(alpha=0.3)(x)
+
+# softmax is applied on last layer
+predictions = Dense(p, activation = 'softmax')(x_1)
 
 #create graph of new model
-head_model = Model(input = vgg19_model.input, output = predictions)
+image_extractor = Model(input = vgg19_model.input, output = predictions)
 
 #compile the model
-head_model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
+image_extractor.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # provide model summary
-head_model.summary()
+image_extractor.summary()
