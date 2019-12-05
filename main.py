e584d59f2359724c407a55cf07d89a824ea5f8b7
Will Patterson <pattwm16@gmail.com>
2019-12-05 18:02:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ sequence_input, concat = text_extract(data_train, data_test, labels_train, label
 # NOTE: Since the size of the fake news detector was relatively small, we did
 # not create a new file for it.
 # Fake News Detector Layers ---
-preds = Dense(1, activation='softmax')(concat) # concat is out from text feature extractor (R^T)
+preds = Dense(1, activation='softmax')(concat) # concat is output from text feature extractor (in R^T space)
 fake_news_detector = Model(sequence_input, preds, name="Fake News Detector")
 
 # Compile and fit fake news detector
@@ -38,6 +38,7 @@ hl_size = 120 # TODO: Tune this hyperparameter
 # HINT: See https://github.com/michetonu/gradient_reversal_keras_tf for implmentation
 ed_fc1_out = Dense(hl_size, activation='relu')(concat)
 ed_fc2_out = Dense(unique_events, activation='softmax')(ed_fc1_out)
+print("unique_events", unique_events)
 event_discrim = Model(sequence_input, ed_fc2_out, name="Event Discriminator")
 
 # Compile and fit the event discriminator
@@ -47,6 +48,9 @@ event_discrim.compile(loss='sparse_categorical_crossentropy',
 print(event_discrim.summary())
 print(len(data_test))
 print(len(subjects_test))
+print(subjects_test.shape)
+# TODO: Fix error here
+# ERROR: ValueError: Error when checking target: expected dense_3 to have shape (1,) but got array with shape (142,)
 event_discrim.fit(data_train, subjects_train,
           batch_size=250,
           epochs=5,
